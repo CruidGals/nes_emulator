2a252479452f60faae898008d55125450b1aa729
CruidGals <kchiem30@gmail.com>
2024-11-23 02:40:05 -0500

    Add rom reader and start PPU

--- emulator_6502/PPU.hpp
+++ emulator_6502/PPU.hpp
@@ -9,5 +9,13 @@
 #define PPU_hpp
 
 #include <stdio.h>
+#include <stdint.h>
+
+typedef struct NESPPU
+{
+    uint8_t *chr_memory; //8KiB Memory
+    uint8_t *work_mem; //2KiB Memory
+    
+} NESPPU;
 
 #endif /* PPU_hpp */
--- emulator_6502/main.cpp
+++ emulator_6502/main.cpp
@@ -6,8 +6,31 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <stdint.h>
 #include "6502emu.hpp"
+
+//Loads hex file into 6502 memory
+void readFile(State6502 *const state, const std::string_view filename, uint32_t offset)
+{
+    //Start reading file through stream in binary mode (start at end of list to get size)
+    std::ifstream file(std::string(filename), std::ios::binary | std::ios::ate);
+    if(!file)
+    {
+        throw std::runtime_error("Could not open file: " + std::string(filename));
+    }
+    
+    //Get size (can do because used std::ios::ate before)
+    std::streamsize size = file.tellg();
+    file.seekg(0, std::ios::beg);
+    
+    //Set position at start of list and read file into memory
+    file.read(reinterpret_cast<char*>(&state->memory[offset]), size);
+    if(!file)
+    {
+        throw std::runtime_error("Could not read entire file");
+    }
+}
 
 State6502* init6502()
 {
@@ -18,13 +41,15 @@ State6502* init6502()
 
 int main(int argc, const char * argv[]) 
 {
+    State6502* state = init6502();
+    
+    /*
     uint8_t program[] = {
         0xA9, 0x01, 0xA2, 0xFF, 0xA0, 0x80, 0x8D, 0x00, 0x02, 0x8E, 0x01, 0x02,
         0x8C, 0x02, 0x02, 0x18, 0x69, 0x02, 0x38, 0xE9, 0x01, 0x29, 0x0F, 0x09,
         0xF0, 0x49, 0xFF, 0xC9, 0x03, 0xF0, 0x02, 0xE8, 0xCA, 0x48, 0x68, 0x60
     };
-    
-    State6502* state = init6502();
+
     
     int counter { 0 };
     
@@ -33,10 +58,15 @@ int main(int argc, const char * argv[])
         state->memory[counter] = opcode;
         counter++;
     }
-
-    for (int i=0; i < counter; i++)
+     */
+    
+    std::string filepath = "./roms/Donkey Kong (Japan).nes";
+    readFile(state, filepath, 0x8000);
+    state->pc = 0x8000;
+    
+    while (state->pc < 0xffff)
     {
-        emulate(state);
+        disassemble(state);
     }
     
 }
